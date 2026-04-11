acd66fbf80f9bb4c23e512a8e42a44674ff286c3
Amishi Arora <amishia859@gmail.com>
2026-04-10 21:44:45 -0700

    Clean up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import FileUpload from "./components/FileUpload";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("http://localhost:3001/")
-      .then((res) => res.text())
-      .then((data) => setMessage(data))
-      .catch((err) => setMessage("Error fetching backend"));
-  }, []);
-
   return (
-    <div className="flex flex-col justify-center gap-20 items-center min-h-screen bg-stone-50 p-15"> 
+    <div className="flex flex-col min-h-screen bg-stone-50 p-15"> 
       <Header />
       <FileUpload/>
     </div>
