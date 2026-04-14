a9b866ade33affff515a61a464f521e9da92a567
Amishi Arora <amishia859@gmail.com>
2026-04-13 19:13:44 -0700

    Added submit exam button

--- client/src/components/Exam.jsx
+++ client/src/components/Exam.jsx
@@ -4,5 +4,6 @@ export default function Exam({ examQuestions }) {
     return <div className="flex flex-col items-center min-h-screen bg-stone-50 p-15 gap-10">
        <Header title = "Practice Exam"/>
         {examQuestions.questions.map((q, i) => <ExamQuestion key = {i} question = {q}/>)}
+        <button className="cursor-pointer bg-blue-500 text-white py-3 px-9 rounded-xl hover:bg-blue-700">Submit Exam</button>
     </div>
 }
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-3.1-flash-lite-preview' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash-lite' });
 const app = express();
 app.use(cors());
 app.use(express.json());
