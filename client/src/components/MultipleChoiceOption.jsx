6205cf099dea9471f91483e4332a1798272a2b98
Amishi Arora <amishia859@gmail.com>
2026-04-23 11:36:18 -0700

    Mark selected MC option as checked on results page

--- client/src/components/MultipleChoiceOption.jsx
+++ client/src/components/MultipleChoiceOption.jsx
@@ -9,7 +9,7 @@ export default function MultipleChoiceOption({ index, id, option, handleAnswer, 
 
     return (
         <div className={`flex border-1 p-1.5 rounded-lg ${optionStyling}`}>
-            <input disabled={isDisabled} onChange={handleAnswer} type="radio" id={`${id} - ${index}`} name={id} className={cursorClass} />
+            <input disabled={isDisabled} onChange={handleAnswer} type="radio" id={`${id} - ${index}`} name={id} checked = {isSelected} className={cursorClass} />
             <label htmlFor={`${id} - ${index}`} className={`font-medium flex-1 ${cursorClass} ml-3`}>{option}</label>
         </div>
     )
